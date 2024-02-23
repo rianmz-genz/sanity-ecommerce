fc61e36ad2eb578b99ed7d931fae70f351602945
rianmz-genz <adrianji1945@gmail.com>
2024-02-23 21:17:46 +0700

    schemas

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,1 +1,13 @@
-export const schemaTypes = []
+import schemas from './schemas'
+export const schemaTypes = [
+  schemas.discount,
+  schemas.order,
+  schemas.orderLine,
+  schemas.payment,
+  schemas.product,
+  schemas.productCategory,
+  schemas.shippingAddress,
+  schemas.user,
+  schemas.cart,
+  schemas.cartItem,
+]
