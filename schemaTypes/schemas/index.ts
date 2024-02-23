b47f207d7ebed0e9c5ed82618bd628e914193577
rianmz-genz <adrianji1945@gmail.com>
2024-02-23 21:19:12 +0700

    schemas

---
+++ schemaTypes/schemas/index.ts
@@ -0,0 +1,23 @@
+import {cart} from './cart'
+import {cartItem} from './cartItem'
+import {discount} from './discount'
+import {order} from './order'
+import {orderLine} from './orderLine'
+import {payment} from './payment'
+import {product} from './product'
+import {productCategory} from './productCategory'
+import {shippingAddress} from './shippingAddress'
+import {user} from './user'
+const schemas = {
+  cart,
+  cartItem,
+  discount,
+  order,
+  orderLine,
+  payment,
+  product,
+  productCategory,
+  shippingAddress,
+  user,
+}
+export default schemas
